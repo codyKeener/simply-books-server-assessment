d345742417bbbb4fb75901e6ce7fbb09f7a89aa5
Cody Keener <codykeener@Codys-MBP.localdomain>
2025-01-11 14:25:58 -0600

    Updated all operations to require a uid in the query parameters. Retrieve and list can still get books and authors without a uid for public books/authors. Also added better error handling for all operations.

--- simplybooksapi/views/author.py
+++ simplybooksapi/views/author.py
@@ -8,71 +8,128 @@ class AuthorView(ViewSet):
   
   def retrieve(self, request, pk):
     
+    # GET THE UID FROM THE REQUEST PARAMETERS
+    uid = request.query_params.get('uid', None)
+    
     try:
-      author = Author.objects.get(pk=pk)
+      # IF THERE IS NOT A UID, GET THE AUTHOR THAT MATCHES THE PK (PUBLIC AUTHORS)
+      if uid is None:
+        author = Author.objects.get(pk=pk)
+      # IF THERE IS A UID, GET THE AUTHOR THAT MATCHES THE PK AND THE UID (PRIVATE AUTHORS)
+      else:
+        author = Author.objects.get(pk=pk, uid=uid)
+        
       book_count = Book.objects.filter(author=author).count()
       author.book_count = book_count
       serializer = SingleAuthorSerializer(author)
       return Response(serializer.data)
+  
     except Author.DoesNotExist as ex:
       return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
     
   def list(self, request):
     
+    # GET THE UID FROM THE REQUEST PARAMETERS
+    uid = request.query_params.get('uid', None)
+    # GET THE FAVORITE VALUE FROM THE REQUEST PARAMETERS
+    favorite = request.query_params.get('favorite', None)
+    
+    # IF TRUE IS PASSED IN THE FAVORITE PARAMETER INSTEAD OF 1, CONVERT TO 1
+    if favorite == True or favorite == 'TRUE' or favorite == 'true':
+      favorite = 1
+      
+    # IF FALSE IS PASSED IN THE FAVORITE PARAMETER INSTEAD OF 0, CONVERT TO 0
+    if favorite == False or favorite == 'FALSE' or favorite == 'false':
+      favorite = 0
+      
     try:
+      authors = Author.objects.all()
       
-      uid = request.query_params.get('uid', None)
-      favorite = request.query_params.get('favorite', None)
-      
-      if favorite is not None and uid is not None:
-        authors = Author.objects.all()
-        authors = authors.filter(uid=uid, favorite=favorite)
-      elif uid is not None:
-        authors = Author.objects.all()
+      # FILTER AUTHORS BY UID IF UID IS PASSED IN THE QUERY PARAMETERS (PRIVATE AUTHORS). IF NO UID IS PASSED IN THE QUERY PARAMETERS, AUTHORS WILL NOT BE FILTERED (PUBLIC AUTHORS)
+      if uid is not None:
         authors = authors.filter(uid=uid)
-      elif favorite is None and uid is None:
-        authors = Author.objects.all()
+        
+      # FILTER AUTHORS BY FAVORITE IF FAVORITE IS PASSED IN THE QUERY PARAMETERS
+      if favorite is not None:
+        # IF THERE IS ONLY A FAVORITE VALUE AND NO UID, THIS WILL RETURN AN ERROR BECAUSE FAVORITE AUTHORS CAN ONLY BE QUERIED FOR SPECIFIC USERS
+        if uid is not None:
+          authors = authors.filter(favorite=favorite)
+        else:
+          return Response({'message': 'Favorite authors can only be queried for specific users'}, status=status.HTTP_403_FORBIDDEN)
       
       serializer = AuthorSerializer(authors, many=True)
       return Response(serializer.data)
-    
-    except :
-        return Response({'message': 'Favorite authors can only be queried for specific users'}, status=status.HTTP_401_UNAUTHORIZED)
+    except:
+      return Response({'message': 'Check query'}, status=status.HTTP_400_BAD_REQUEST)
   
   def create(self, request):
     
-    author = Author.objects.create(
-      email=request.data["email"],
-      first_name=request.data["first_name"],
-      last_name=request.data["last_name"],
-      image=request.data["image"],
-      favorite=request.data["favorite"],
-      uid=request.data["uid"]
-    )
-    serializer = AuthorSerializer(author)
-    return Response(serializer.data, status=status.HTTP_201_CREATED)
+    try:
+      author = Author.objects.create(
+        email=request.data["email"],
+        first_name=request.data["first_name"],
+        last_name=request.data["last_name"],
+        image=request.data["image"],
+        favorite=request.data["favorite"],
+        uid=request.data["uid"]
+      )
+      serializer = AuthorSerializer(author)
+      return Response(serializer.data, status=status.HTTP_201_CREATED)
+    except:
+      # IF ALL VALUES ARE NOT PRESENT IN THE REQUEST, THE AUTHOR WILL NOT BE CREATED
+      return Response({'message': 'All data points must be provided: email, first_name, last_name, image, favorite, uid'}, status=status.HTTP_417_EXPECTATION_FAILED)
   
   def update(self, request, pk):
     
+    # GET THE UID FROM THE REQUEST PARAMETERS
+    uid = request.query_params.get('uid', None)
+    
     id = pk
     author = Author.objects.get(pk=pk)
-    author.email=request.data["email"]
-    author.first_name = request.data["first_name"]
-    author.last_name = request.data["last_name"]
-    author.image = request.data["image"]
-    author.favorite = request.data["favorite"]
-    author.uid = request.data["uid"]
     
-    author.save()
-    
-    serializer = AuthorSerializer(author)    
-    return Response(serializer.data, status=status.HTTP_200_OK)
+    # IF A UID IS NOT PASSED IN THE QUERY PARAMETERS, RETURN AN ERROR BECAUSE WE NEED TO VERIFY THAT THE USER REQUESTING TO UPDATE THE AUTHOR IS THE USER THAT CREATED THE AUTHOR (PRIVATE AUTHORS)
+    if uid == None:
+      return Response({'message': 'Must provide the uid of the user requesting to update the author in the query parameters'}, status=status.HTTP_403_FORBIDDEN)
+    # IF THE UID PASSED IN THE QUERY PARAMETERS IS THE SAME AS THE UID OF THE AUTHOR, UPDATE THE AUTHOR (PRIVATE AUTHORS)
+    elif author.uid == uid:
+      try:
+        author.email=request.data["email"]
+        author.first_name = request.data["first_name"]
+        author.last_name = request.data["last_name"]
+        author.image = request.data["image"]
+        author.favorite = request.data["favorite"]
+        author.uid = uid
+        
+        author.save()
+        
+        serializer = AuthorSerializer(author)    
+        return Response(serializer.data, status=status.HTTP_200_OK)
+      except:
+        # IF ALL VALUES ARE NOT PRESENT IN THE REQUEST, THE AUTHOR WILL NOT BE UPDATED
+        return Response({'message': 'All data points must be provided: email, first_name, last_name, image, favorite, uid'}, status=status.HTTP_417_EXPECTATION_FAILED)
+    else:
+      # IF THE UID IN THE QUERY PARAMETERS DOES NOT MATCH THE UID OF THE AUTHOR, RETURN AN ERROR BECAUSE THE USER THAT IS REQUESTING TO UPDATE THE AUTHOR MUST BE THE USER THAT CREATED THE AUTHOR (PRIVATE AUTHORS)
+      return Response({'message': 'Only the user that created the author can update it'}, status=status.HTTP_403_FORBIDDEN)
   
   def destroy(self, request, pk):
     
+    # GET THE UID FROM THE REQUEST PARAMETERS
+    uid = request.query_params.get('uid', None)
+    
     author = Author.objects.get(pk=pk)
-    author.delete()
-    return Response(None, status=status.HTTP_204_NO_CONTENT)
+    
+    if uid == None:
+      return Response({'message': 'Must provide the uid of the user requesting to delete the author in the query parameters'}, status=status.HTTP_403_FORBIDDEN)
+    # IF THE UID PASSED IN THE QUERY PARAMETERS IS THE SAME AS THE UID OF THE AUTHOR, DELETE THE AUTHOR (PRIVATE AUTHORS)
+    elif author.uid == uid:
+      try:
+        author.delete()
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+      except:
+        return Response({'message': 'Check query'}, status=status.HTTP_400_BAD_REQUEST)
+    else:
+      # IF THE UID IN THE QUERY PARAMETERS DOES NOT MATCH THE UID OF THE AUTHOR, RETURN AN ERROR BECAUSE THE USER THAT IS REQUESTING TO DELETE THE AUTHOR MUST BE THE USER THAT CREATED THE AUTHOR (PRIVATE AUTHORS)
+      return Response({'message': 'Only the user that created the author can delete it'}, status=status.HTTP_403_FORBIDDEN)
   
 class AuthorSerializer(serializers.ModelSerializer):
   
--- simplybooksapi/views/book.py
+++ simplybooksapi/views/book.py
@@ -8,73 +8,132 @@ class BookView(ViewSet):
   
   def retrieve(self, request, pk):
     
+    # GET THE UID FROM THE REQUEST PARAMETERS
+    uid = request.query_params.get('uid', None)
+    
+    
     try:
-      book = Book.objects.get(pk=pk)
+    # IF THERE IS NOT A UID, GET THE BOOK THAT MATCHES THE PK (PUBLIC BOOKS)
+      if uid is None:
+        book = Book.objects.get(pk=pk)
+    # IF THERE IS A UID, GET THE BOOK THAT MATCHES THE PK AND THE UID (PRIVATE BOOKS)
+      else:
+        book = Book.objects.get(pk=pk, uid=uid)
+      
       serializer = BookSerializer(book)
       return Response(serializer.data)
+    
     except Book.DoesNotExist as ex:
-      return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+      return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)    
     
   def list(self, request):
     
-    uid = request.query_params.get('uid', None)
+    # GET THE AUTHOR FROM THE REQUEST PARAMETERS
     author = request.query_params.get('author', None)
+    # GET THE UID FROM THE REQUEST PARAMETERS
+    uid = request.query_params.get('uid', None)
+    # GET THE SALE VALUE FROM THE REQUEST PARAMETERS
+    sale = request.query_params.get('sale', None)
     
-    if author is not None and uid is not None:
+    # IF TRUE IS PASSED IN THE FAVORITE PARAMETER INSTEAD OF 1, CONVERT TO 1
+    if sale == True or sale == 'TRUE' or sale == 'true':
+      sale = 1
+    
+    # IF FALSE IS PASSED IN THE FAVORITE PARAMETER INSTEAD OF 0, CONVERT TO 0
+    if sale == False or sale == 'FALSE' or sale == 'false':
+      sale = 0
+    
+    try: 
       books = Book.objects.all()
-      books = books.filter(uid=uid, author=author)
-    elif uid is not None:
-      books = Book.objects.all()
-      books = books.filter(uid=uid)
-    elif author is not None:
-      books = Book.objects.all()
-      books = books.filter(author=author)
-    elif author is None and uid is None:
-      books = Book.objects.all()
+      
+      # FILTER BOOKS BY UID IF UID IS PASSED IN THE QUERY PARAMETERS (PRIVATE BOOKS)
+      if uid is not None:
+        books = books.filter(uid=uid)
+      
+      # FILTER BOOKS BY AUTHOR IF AUTHOR IS PASSED IN THE QUERY PARAMETERS (WORKS FOR PUBLIC OR PRIVATE BOOKS)
+      if author is not None:
+        books = books.filter(author=author)
+      
+      # FILTER BOOKS BY SALE IF SALE IS PASSED IN THE QUERY PARAMETERS (WORKS FOR PUBLIC OR PRIVATE BOOKS)  
+      if sale is not None:
+        books = books.filter(sale=sale)
         
-    serializer = BookSerializer(books, many=True)
-    return Response(serializer.data)
+      serializer = BookSerializer(books, many=True)
+      return Response(serializer.data)
+    except:
+      return Response({'message': 'Check query'}, status=status.HTTP_400_BAD_REQUEST)
   
   def create(self, request):
     
     author = Author.objects.get(pk=request.data["author_id"])
     
-    book = Book.objects.create(
-      author=author,
-      title=request.data["title"],
-      image=request.data["image"],
-      price=request.data["price"],
-      sale=request.data["sale"],
-      uid=request.data["uid"],
-      description=request.data["description"]
-    )
-    serializer = BookSerializer(book)
-    return Response(serializer.data, status=status.HTTP_201_CREATED)
+    try:
+      book = Book.objects.create(
+        author=author,
+        title=request.data["title"],
+        image=request.data["image"],
+        price=request.data["price"],
+        sale=request.data["sale"],
+        uid=request.data["uid"],
+        description=request.data["description"]
+      )
+      serializer = BookSerializer(book)
+      return Response(serializer.data, status=status.HTTP_201_CREATED)
+    except:
+      # IF ALL VALUES ARE NOT PRESENT IN THE REQUEST, THE BOOK WILL NOT BE CREATED
+      return Response({'message': 'All data points must be provided: author_id, title, image, price, sale, uid, description'}, status=status.HTTP_417_EXPECTATION_FAILED)
   
   def update(self, request, pk):
+    
+    # GET THE UID FROM THE REQUEST PARAMETERS
+    uid = request.query_params.get('uid', None)
     
     author = Author.objects.get(pk=request.data["author_id"])
     
     id = pk
     book = Book.objects.get(pk=pk)
-    book.author=author
-    book.title = request.data["title"]
-    book.image = request.data["image"]
-    book.price = request.data["price"]
-    book.sale = request.data["sale"]
-    book.uid = request.data["uid"]
-    book.description=request.data["description"]
     
-    book.save()
-    
-    serializer = BookSerializer(book)    
-    return Response(serializer.data, status=status.HTTP_200_OK)
-  
+    # IF A UID IS NOT PASSED IN THE QUERY PARAMETERS, RETURN AN ERROR BECAUSE WE NEED TO VERIFY THAT THE USER REQUESTING TO UPDATE THE BOOK IS THE USER THAT CREATED THE BOOK (PRIVATE BOOKS)
+    if uid == None:
+      return Response({'message': 'Must provide the uid of the user requesting to update the book in the query parameters'}, status=status.HTTP_403_FORBIDDEN)
+    # IF THE UID PASSED IN THE QUERY PARAMETERS IS THE SAME AS THE UID OF THE BOOK, UPDATE THE BOOK (PRIVATE BOOKS)
+    elif book.uid == uid:
+      try:
+        book.author=author
+        book.title = request.data["title"]
+        book.image = request.data["image"]
+        book.price = request.data["price"]
+        book.sale = request.data["sale"]
+        book.uid = request.data["uid"]
+        book.description=request.data["description"]
+        
+        book.save()
+        
+        serializer = BookSerializer(book)    
+        return Response(serializer.data, status=status.HTTP_200_OK)
+      except:
+        # IF ALL VALUES ARE NOT PRESENT IN THE REQUEST, THE BOOK WILL NOT BE UPDATED
+        return Response({'message': 'All data points must be provided: author_id, title, image, price, sale, uid, description'}, status=status.HTTP_417_EXPECTATION_FAILED)
+      
   def destroy(self, request, pk):
     
+    # GET THE UID FROM THE REQUEST PARAMETERS
+    uid = request.query_params.get('uid', None)
+    
     book = Book.objects.get(pk=pk)
-    book.delete()
-    return Response(None, status=status.HTTP_204_NO_CONTENT)
+    
+    if uid == None:
+      return Response({'message': 'Must provide the uid of the user requesting to delete the book in the query parameters'}, status=status.HTTP_403_FORBIDDEN)
+    # IF THE UID PASSED IN THE QUERY PARAMETERS IS THE SAME AS THE UID OF THE BOOK, DELETE THE BOOK (PRIVATE BOOKS)
+    elif book.uid == uid:
+      try:
+        book.delete()
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+      except:
+        return Response({'message': 'Check query'}, status=status.HTTP_400_BAD_REQUEST)
+    else:
+      # IF THE UID IN THE QUERY PARAMETERS DOES NOT MATCH THE UID OF THE BOOK, RETURN AN ERROR BECAUSE THE USER THAT IS REQUESTING TO DELETE THE BOOK MUST BE THE USER THAT CREATED THE BOOK (PRIVATE BOOKS)
+      return Response({'message': 'Only the user that created the author can delete it'}, status=status.HTTP_403_FORBIDDEN)
   
 class BookSerializer(serializers.ModelSerializer):
   
